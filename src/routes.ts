09f5ced2eaa01199757c017dddb75a38129459a0
Danilo Rocha <danilosilvasrocha@gmail.com>
2023-01-27 13:53:44 -0300

    Add: System of Image and more Services

--- src/routes.ts
+++ src/routes.ts
@@ -1,25 +1,37 @@
 import { Router } from 'express';
+import multer from 'multer';
 
-import { AuthUserController } from './controllers/user/AuthUserController';
 import { CreateUserController } from './controllers/user/CreateUserController'
-import { DetailUserController } from './controllers/user/DetailUserController';
-import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { AuthUserController } from './controllers/user/AuthUserController'
+import { DetailUserController } from './controllers/user/DetailUserController'
 
-import { isAuthenticated } from './middlewares/isAuthenticated';
-import { ListCategoryController } from './controllers/category/ListCategoryController';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+import { ListCategoryController } from './controllers/category/ListCategoryController'
+
+import { CreateProductController } from './controllers/products/CreateProductController';
+
+import { isAuthenticated } from './middlewares/isAuthenticated'
+
+import uploadConfig from './config/multer'
+
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
 
 //-- ROTAS USER --
 router.post('/users', new CreateUserController().handle)
 
 router.post('/session', new AuthUserController().handle)
 
-router.get('/userInfo', isAuthenticated, new DetailUserController().handle)
+router.get('/me', isAuthenticated, new DetailUserController().handle)
 
-//-- ROTAS CATEGORY --
-
+//-- ROTAS CATEGORY
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
-router.get('/categories', isAuthenticated, new ListCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+//-- ROTAS PRODUCT
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
+
 
 export { router }; 
